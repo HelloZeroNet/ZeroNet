0601d7bf9eb50da8cdf3c2001ff0db2927c0b2cb
Marcus McCurdy <marcus.mccurdy@gmail.com>
2015-01-26 15:48:55 -0500

    Allows specifying the browser name to launch

--- start.py
+++ start.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 from multiprocessing import Process
+import sys
 import webbrowser
 import zeronet
 
-server = Process(target=zeronet.main)
-server.start()
-url = webbrowser.open("http://127.0.0.1:43110", new=2)
-server.join()
+def main():
+    browser_name = sys.argv.pop() if len(sys.argv) >= 2 else None
+    server = Process(target=zeronet.main)
+    server.start()
+    browser = webbrowser.get(browser_name)
+    url = browser.open("http://127.0.0.1:43110", new=2)
+    server.join()
+
+if __name__ == '__main__':
+    main()
