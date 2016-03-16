3a8e20df7de099bca9a7a3998ec8bd9f89b105c4
HelloZeroNet <hello@noloop.me>
2016-03-16 21:12:04 +0100

    Rev1033

--- src/Config.py
+++ src/Config.py
@@ -8,7 +8,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.3.6"
-        self.rev = 1026
+        self.rev = 1033
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
