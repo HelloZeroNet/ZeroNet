a40580501ae7dffe4841a5bc028d1c38b0253f70
HelloZeroNet <hello@noloop.me>
2016-04-10 23:12:50 +0200

    Version 0.3.7, Rev1210

--- src/Config.py
+++ src/Config.py
@@ -7,8 +7,8 @@ import ConfigParser
 class Config(object):
 
     def __init__(self, argv):
-        self.version = "0.3.6"
-        self.rev = 1209
+        self.version = "0.3.7"
+        self.rev = 1210
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
