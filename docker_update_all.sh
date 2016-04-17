d07de66055a1a94c275e33669156e047bab714d7
Gyula Weber <gyula.weber@liferay.com>
2016-04-17 17:29:07 +0200

    added tor install and can be switched on/off with env variables

---
+++ docker_update_all.sh
@@ -0,0 +1,33 @@
+# update sources
+# rebuild container
+# stop old
+# start new
+# display logs (feel free to ctrl-c)
+
+#!/bin/bash
+
+echo "updating repository"
+git pull origin master
+
+echo "building container"
+docker build -t zeronet_tor .
+
+echo "stopping old container"
+docker stop zeronet_t
+
+echo "removing old container"
+docker rm -v zeronet_t
+
+echo "starting new container"
+docker run -d  \
+  -e START_TOR=1 \
+  -v $(pwd)/../zeronet:/root/data \
+  --name zeronet_t \
+  -p 15441:15441 \
+  -p 43110:43110 \
+  zeronet_tor
+
+echo "--- displaying logs ---"
+sleep 1
+
+docker logs -f zeronet_t
