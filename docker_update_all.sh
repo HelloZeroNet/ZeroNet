8a09b194c0782b8c029dd384824247b037b8efb3
Gyula Weber <gyula.weber@liferay.com>
2016-04-18 15:43:47 +0200

    dont start tor for me

--- docker_update_all.sh
+++ docker_update_all.sh
@@ -10,11 +10,7 @@ echo "updating repository"
 git pull origin master
 
 echo "building container"
-<<<<<<< HEAD
-docker build --no-cache -t zeronet_tor .
-=======
 docker build -t zeronet_tor .
->>>>>>> d07de66055a1a94c275e33669156e047bab714d7
 
 echo "stopping old container"
 docker stop zeronet_t
@@ -24,11 +20,7 @@ docker rm -v zeronet_t
 
 echo "starting new container"
 docker run -d  \
-<<<<<<< HEAD
   -e START_TOR=0 \
-=======
-  -e START_TOR=1 \
->>>>>>> d07de66055a1a94c275e33669156e047bab714d7
   -v $(pwd)/../zeronet:/root/data \
   --name zeronet_t \
   -p 15441:15441 \
