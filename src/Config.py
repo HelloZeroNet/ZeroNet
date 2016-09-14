e7762e03b44c20a08653e6e0f4a9d94a69600f38
shortcutme <hello@noloop.me>
2016-09-14 10:54:33 +0200

    Rev1521

--- src/Config.py
+++ src/Config.py
@@ -8,7 +8,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.4.1"
-        self.rev = 1518
+        self.rev = 1521
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
