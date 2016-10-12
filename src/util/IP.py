0de0990df2ab8693ece9438e23316d597257d748
Jeff Becker <ampernand@gmail.com>
2016-10-12 10:14:04 -0400

    check for all cases

--- src/util/IP.py
+++ src/util/IP.py
@@ -1,4 +1,4 @@
-
+import string
 
 def is_private_address(ip):
     """
@@ -6,5 +6,12 @@ def is_private_address(ip):
     """
     if isinstance(ip, tuple):
         ip = ip[0]
-    prefix = ip.split(".")[0]
+    split = ip.split(".")
+    if len(split) != 4:
+        return False
+    for part in split:
+        for c in part:
+            if c not in string.digits:
+                return False
+    prefix = split[0]
     return prefix in ['127', '10']
