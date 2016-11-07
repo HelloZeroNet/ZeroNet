fc239cfd0d12fbddb51d2d9f9c760e02ddcb7a18
shortcutme <hello@noloop.me>
2016-11-07 22:52:03 +0100

    Change tracker to more reliable one

--- src/Config.py
+++ src/Config.py
@@ -36,7 +36,7 @@ class Config(object):
             "udp://tracker.coppersurfer.tk:6969",
             "udp://tracker.leechers-paradise.org:6969",
             "udp://9.rarbg.com:2710",
-            "http://tracker.aletorrenty.pl:2710/announce",
+            "http://tracker.tordb.ml:6881/announce",
             "http://explodie.org:6969/announce",
             "http://tracker1.wasabii.com.tw:6969/announce"
         ]
