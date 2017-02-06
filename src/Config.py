d411d4d3981c850472288fe97de0601855edf294
shortcutme <tamas@zeronet.io>
2017-02-06 09:57:51 +0100

    Rev1862, Skip optional file delete on invalid limit

--- src/Config.py
+++ src/Config.py
@@ -10,7 +10,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.5.1"
-        self.rev = 1861
+        self.rev = 1862
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
