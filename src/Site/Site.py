4aff1468128f4947b440def845ec30c196c5302c
shortcutme <tamas@zeronet.io>
2017-02-22 11:28:17 +0100

    Log giving up on a file

--- src/Site/Site.py
+++ src/Site/Site.py
@@ -1061,4 +1061,5 @@ class Site(object):
         self.updateWebsocket(file_failed=inner_path)
 
         if self.bad_files.get(inner_path, 0) > 30:
+            self.log.debug("Giving up on %s" % inner_path)
             del self.bad_files[inner_path]  # Give up after 30 tries
