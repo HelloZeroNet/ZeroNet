a72c4eafff58e12cac4acd13fad67e3717ce4de8
shortcutme <tamas@zeronet.io>
2017-02-27 00:14:19 +0100

    Version 0.5.3, Rev1988

--- src/Config.py
+++ src/Config.py
@@ -9,8 +9,8 @@ import ConfigParser
 class Config(object):
 
     def __init__(self, argv):
-        self.version = "0.5.2"
-        self.rev = 1941
+        self.version = "0.5.3"
+        self.rev = 1988
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
