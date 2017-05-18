e6680b4f60aec5d3fa576413d207a7842600edad
shortcutme <tamas@zeronet.io>
2017-05-18 23:03:26 +0200

    Version 0.5.5, Rev2081, Lack of JS notification

--- src/Config.py
+++ src/Config.py
@@ -9,8 +9,8 @@ import ConfigParser
 class Config(object):
 
     def __init__(self, argv):
-        self.version = "0.5.4"
-        self.rev = 2080
+        self.version = "0.5.5"
+        self.rev = 2081
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
