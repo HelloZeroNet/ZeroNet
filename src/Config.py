653219d1de0345035a7ccdb5ed236487c50809a0
shortcutme <tamas@zeronet.io>
2017-06-03 00:54:26 +0200

    Rev2096

--- src/Config.py
+++ src/Config.py
@@ -10,7 +10,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.5.5"
-        self.rev = 2092
+        self.rev = 2096
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
