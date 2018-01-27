d44677e46f83d436012524498d2219128cc5067c
shortcutme <tamas@zeronet.io>
2018-01-27 12:21:25 +0100

    Don't keep connections for sites that not modified in last week

--- src/File/FileServer.py
+++ src/File/FileServer.py
@@ -274,9 +274,12 @@ class FileServer(ConnectionServer):
                     site.retryBadFiles()
 
                 if not startup:  # Don't do it at start up because checkSite already has needConnections at start up.
-                    connected_num = site.needConnections(check_site_on_reconnect=True)  # Keep active peer connection to get the updates
-                    if connected_num < config.connected_limit:  # This site has small amount of peers, protect them from closing
-                        peers_protected.update([peer.key for peer in site.getConnectedPeers()])
+                    if time.time() - site.settings.get("modified") < 60 * 60 * 24 * 7:
+                        # Keep active connections if site has been modified witin 7 days
+                        connected_num = site.needConnections(check_site_on_reconnect=True)
+
+                        if connected_num < config.connected_limit:  # This site has small amount of peers, protect them from closing
+                            peers_protected.update([peer.key for peer in site.getConnectedPeers()])
 
                 time.sleep(1)  # Prevent too quick request
 
