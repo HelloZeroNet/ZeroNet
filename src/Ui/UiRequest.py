d275dfea2fb916492bee8a87934f167e583c2a3d
shortcutme <tamas@zeronet.io>
2018-03-20 21:55:45 +0100

    Log 403 error as error

--- src/Ui/UiRequest.py
+++ src/Ui/UiRequest.py
@@ -678,7 +678,7 @@ class UiRequest(object):
     # You are not allowed to access this
     def error403(self, message="", details=True):
         self.sendHeader(403)
-        self.log.debug("Error 403: %s" % message)
+        self.log.error("Error 403: %s" % message)
         return self.formatError("Forbidden", message, details=details)
 
     # Send file not found error
