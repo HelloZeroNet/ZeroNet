fd842c43a6bd36321653906608b53d44d970e24e
shortcutme <tamas@zeronet.io>
2018-04-18 13:13:20 +0200

    Rev3412, Change no longer working trackers

--- src/Config.py
+++ src/Config.py
@@ -10,7 +10,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.6.2"
-        self.rev = 3411
+        self.rev = 3412
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
@@ -37,10 +37,10 @@ class Config(object):
             "zero://zero.booth.moe#f36ca555bee6ba216b14d10f38c16f7769ff064e0e37d887603548cc2e64191d:443",  # US/NY
             "udp://tracker.coppersurfer.tk:6969",  # DE
             "udp://tracker.leechers-paradise.org:6969",  # NL
-            "udp://thetracker.org:80",  # FR
-            "http://torrentsmd.eu:8080/announce",  # US?/Cloudflare
+            "udp://104.238.198.186:8000",  # US/LA
+            "http://tracker.skyts.net:6969/announce",  # CN
             "http://0d.kebhana.mx:443/announce",  # FR
-            "http://retracker.spark-rostov.ru:80/announce"  # RU
+            "http://retracker.mgts.by:80/announce"  # BY
         ]
         # Platform specific
         if sys.platform.startswith("win"):
