0244db918291107254e68c5c16e68d792e9f8d01
shortcutme <tamas@zeronet.io>
2018-04-28 21:43:22 +0200

    Fix config save line parsing without values

--- src/Config.py
+++ src/Config.py
@@ -416,7 +416,7 @@ class Config(object):
         for line in lines:
             if line.strip() == "[global]":
                 global_line_i = i
-            if line.startswith(key + " = "):
+            if line.startswith(key + " ="):
                 key_line_i = i
             i += 1
 
