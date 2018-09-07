84ab076fe8abfb163f58c96589bdbadd884ce847
shortcutme <tamas@zeronet.io>
2018-09-07 11:34:11 +0200

    Rev3594

--- src/Config.py
+++ src/Config.py
@@ -10,7 +10,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.6.3"
-        self.rev = 3591
+        self.rev = 3594
         self.argv = argv
         self.action = None
         self.pending_changes = {}
