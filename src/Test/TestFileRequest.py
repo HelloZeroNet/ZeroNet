933422e70477f77fcd435d06db4e1b475aab6fe1
tangdou1 <35254744+tangdou1@users.noreply.github.com>
2018-09-22 10:31:31 +0800

    Update TestFileRequest.py

--- src/Test/TestFileRequest.py
+++ src/Test/TestFileRequest.py
@@ -51,10 +51,81 @@ class TestFileRequest:
         connection.close()
         client.stop()
 
+    def testGetFile6(self, file_server, site):
+        file_server.ip_incoming = {}  # Reset flood protection
+        client = ConnectionServer("0:0:0:0:0:0:0:1", 1545)
+
+        connection = client.getConnection("0:0:0:0:0:0:0:1", 1544)
+        file_server.sites[site.address] = site
+
+        # Normal request
+        response = connection.request("getFile", {"site": site.address, "inner_path": "content.json", "location": 0})
+        assert "sign" in response["body"]
+
+        response = connection.request("getFile", {"site": site.address, "inner_path": "content.json", "location": 0, "file_size": site.storage.getSize("content.json")})
+        assert "sign" in response["body"]
+
+        # Invalid file
+        response = connection.request("getFile", {"site": site.address, "inner_path": "invalid.file", "location": 0})
+        assert "File read error" in response["error"]
+
+        # Location over size
+        response = connection.request("getFile", {"site": site.address, "inner_path": "content.json", "location": 1024 * 1024})
+        assert "File read error" in response["error"]
+
+        # Stream from parent dir
+        response = connection.request("getFile", {"site": site.address, "inner_path": "../users.json", "location": 0})
+        assert "File read error" in response["error"]
+
+        # Invalid site
+        response = connection.request("getFile", {"site": "", "inner_path": "users.json", "location": 0})
+        assert "Unknown site" in response["error"]
+
+        response = connection.request("getFile", {"site": ".", "inner_path": "users.json", "location": 0})
+        assert "Unknown site" in response["error"]
+
+        # Invalid size
+        response = connection.request("getFile", {"site": site.address, "inner_path": "content.json", "location": 0, "file_size": 1234})
+        assert "File size does not match" in response["error"]
+
+        connection.close()
+        client.stop()
+
     def testStreamFile(self, file_server, site):
         file_server.ip_incoming = {}  # Reset flood protection
         client = ConnectionServer("127.0.0.1", 1545)
         connection = client.getConnection("127.0.0.1", 1544)
+        file_server.sites[site.address] = site
+
+        buff = StringIO.StringIO()
+        response = connection.request("streamFile", {"site": site.address, "inner_path": "content.json", "location": 0}, buff)
+        assert "stream_bytes" in response
+        assert "sign" in buff.getvalue()
+
+        # Invalid file
+        buff = StringIO.StringIO()
+        response = connection.request("streamFile", {"site": site.address, "inner_path": "invalid.file", "location": 0}, buff)
+        assert "File read error" in response["error"]
+
+        # Location over size
+        buff = StringIO.StringIO()
+        response = connection.request(
+            "streamFile", {"site": site.address, "inner_path": "content.json", "location": 1024 * 1024}, buff
+        )
+        assert "File read error" in response["error"]
+
+        # Stream from parent dir
+        buff = StringIO.StringIO()
+        response = connection.request("streamFile", {"site": site.address, "inner_path": "../users.json", "location": 0}, buff)
+        assert "File read error" in response["error"]
+
+        connection.close()
+        client.stop()
+
+    def testStreamFile6(self, file_server, site):
+        file_server.ip_incoming = {}  # Reset flood protection
+        client = ConnectionServer("0:0:0:0:0:0:0:1", 1545)
+        connection = client.getConnection("0:0:0:0:0:0:0:1", 1544)
         file_server.sites[site.address] = site
 
         buff = StringIO.StringIO()
@@ -113,6 +184,39 @@ class TestFileRequest:
         assert not peer_file_server.pex()
         assert "192.168.0.1:11337" not in site_temp.peers
 
+        connection.close()
+        client.stop()
+
+    def testPex6(self, file_server, site, site_temp):
+        file_server.sites[site.address] = site
+        client = FileServer("0:0:0:0:0:0:0:1", 1545)
+        client.sites[site_temp.address] = site_temp
+        site_temp.connection_server = client
+        connection = client.getConnection("0:0:0:0:0:0:0:1", 1544)
+
+        # Add new fake peer to site
+        fake_peer = site.addPeer("1:2:3:4:5:6:7:8", 11337, return_peer=True)
+        # Add fake connection to it
+        fake_peer.connection = Connection(file_server, "1:2:3:4:5:6:7:8", 11337)
+        fake_peer.connection.last_recv_time = time.time()
+        assert fake_peer in site.getConnectablePeers()
+
+        # Add file_server as peer to client
+        peer_file_server = site_temp.addPeer("0:0:0:0:0:0:0:1", 1544)
+
+        assert "1:2:3:4:5:6:7:8:11337" not in site_temp.peers
+        assert peer_file_server.pex()
+        assert "1:2:3:4:5:6:7:8:11337" in site_temp.peers
+
+        # Should not exchange private peers from local network
+        fake_peer_private = site.addPeer("192.168.0.1", 11337, return_peer=True)
+        assert fake_peer_private not in site.getConnectablePeers(allow_private=False)
+        fake_peer_private.connection = Connection(file_server, "192.168.0.1", 11337)
+        fake_peer_private.connection.last_recv_time = time.time()
+
+        assert "192.168.0.1:11337" not in site_temp.peers
+        assert not peer_file_server.pex()
+        assert "192.168.0.1:11337" not in site_temp.peers
 
         connection.close()
         client.stop()
