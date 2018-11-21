293afa60598943102ec1fc49a3d97b93371cd059
shortcutme <tamas@zeronet.io>
2018-11-21 03:36:55 +0100

    Replace dead trackers

--- src/Config.py
+++ src/Config.py
@@ -77,9 +77,9 @@ class Config(object):
             "zero://boot3rdez4rzn36x.onion:15441",
             "zero://zero.booth.moe#f36ca555bee6ba216b14d10f38c16f7769ff064e0e37d887603548cc2e64191d:443",  # US/NY
             "udp://tracker.coppersurfer.tk:6969",  # DE
-            "udp://5.79.83.193:6969",  # NL
+            "udp://tracker.port443.xyz:6969",  # UK
             "udp://104.238.198.186:8000",  # US/LA
-            "http://tracker.swateam.org.uk:2710/announce",  # US/NY
+            "http://tracker2.itzmx.com:6961/announce",  # US/LA
             "http://open.acgnxtracker.com:80/announce",  # DE
             "http://retracker.mgts.by:80/announce"  # BY
         ]
