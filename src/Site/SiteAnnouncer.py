e7de562b93763cc7c1e563efca60468935b6778c
shortcutme <tamas@zeronet.io>
2019-01-20 16:27:59 +0100

    Don't request ipv6 trackers if not supported by the client

--- src/Site/SiteAnnouncer.py
+++ src/Site/SiteAnnouncer.py
@@ -46,6 +46,9 @@ class SiteAnnouncer(object):
         if not self.site.connection_server.tor_manager.enabled:
             trackers = [tracker for tracker in trackers if ".onion" not in tracker]
 
+        if "ipv6" not in self.site.connection_server.supported_ip_types:
+            trackers = [tracker for tracker in trackers if helper.getIpType(self.getAddressParts(tracker)["ip"]) != "ipv6"]
+
         return trackers
 
     def getAnnouncingTrackers(self, mode):
