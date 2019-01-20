8384c2cba349b2ee6b61b763728e39737f84450b
shortcutme <tamas@zeronet.io>
2019-01-20 19:11:15 +0100

    Ipv6 compatible AnnounceShare test

--- plugins/AnnounceShare/Test/TestAnnounceShare.py
+++ plugins/AnnounceShare/Test/TestAnnounceShare.py
@@ -1,29 +1,25 @@
-import time
-import copy
-
-import gevent
 import pytest
-import mock
 
 from AnnounceShare import AnnounceSharePlugin
-from File import FileServer
 from Peer import Peer
-from Test import Spy
+from Config import config
 
 
 @pytest.mark.usefixtures("resetSettings")
 @pytest.mark.usefixtures("resetTempSettings")
 class TestAnnounceShare:
     def testAnnounceList(self, file_server):
-        peer = Peer("127.0.0.1", 1544, connection_server=file_server)
+        open("%s/trackers.json" % config.data_dir, "w").write("{}")
+        tracker_storage = AnnounceSharePlugin.tracker_storage
+        tracker_storage.load()
+        print tracker_storage.file_path, config.data_dir
+        peer = Peer(file_server.ip, 1544, connection_server=file_server)
         assert peer.request("getTrackers")["trackers"] == []
 
-        tracker_storage = AnnounceSharePlugin.tracker_storage
-        tracker_storage.onTrackerFound("zero://127.0.0.1:15441")
+        tracker_storage.onTrackerFound("zero://%s:15441" % file_server.ip)
         assert peer.request("getTrackers")["trackers"] == []
 
         # It needs to have at least one successfull announce to be shared to other peers
-        tracker_storage.onTrackerSuccess("zero://127.0.0.1:15441", 1.0)
-        assert peer.request("getTrackers")["trackers"] == ["zero://127.0.0.1:15441"]
-
+        tracker_storage.onTrackerSuccess("zero://%s:15441" % file_server.ip, 1.0)
+        assert peer.request("getTrackers")["trackers"] == ["zero://%s:15441" % file_server.ip]
 
