c03cd02eed99d64bcfb14930986e224b1f9a21df
blurHY <blurhy@outlook.com>
2019-01-29 12:35:03 +0800

    Fix atexit not working on SIGTERM

--- zeronet.py
+++ zeronet.py
@@ -11,6 +11,11 @@ def main():
 
     main = None
     try:
+        import atexit
+        from signal import signal, SIGTERM
+
+        signal(SIGTERM, lambda signum, stack_frame: sys.exit(0))
+        
         app_dir = os.path.dirname(os.path.abspath(__file__))
         os.chdir(app_dir)  # Change working dir to zeronet.py dir
         sys.path.insert(0, os.path.join(app_dir, "src/lib"))  # External liblary directory
