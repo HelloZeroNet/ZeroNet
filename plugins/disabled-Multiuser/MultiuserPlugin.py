15d2fd45813a029f0c84e4e80dd17b4184aff4f4
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2019-02-15 21:19:29 +0000

    Remove unnecessary str replace
    Presumably left over from before it got moved out into a script.

--- plugins/disabled-Multiuser/MultiuserPlugin.py
+++ plugins/disabled-Multiuser/MultiuserPlugin.py
@@ -193,7 +193,7 @@ class UiWebsocketPlugin(object):
             <a href='#' class='button' style='margin-left: 0px'>Ok, Saved it!</a><br><br>
             <small>This site allows you to browse ZeroNet content, but if you want to secure your account <br>
             and help to keep the network alive, then please run your own <a href='https://zeronet.io' target='_blank'>ZeroNet client</a>.</small>
-        """.replace("{master_seed}", master_seed)
+        """
 
         self.cmd("notification", ["info", message])
 
