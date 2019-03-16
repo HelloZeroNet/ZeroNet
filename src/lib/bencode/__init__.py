3eae349a0ac28f94d19b4b971e8fae43f758a03d
shortcutme <tamas@zeronet.io>
2019-03-16 02:58:49 +0100

    Remove included win_inet_pton, websocket, rsa, socks, pyelliptic, pybitcointools, pyasn1, opensslVerify, merkletools, geventwebsocket, BitcoinECC, bencode module

--- src/lib/bencode/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    string_type = basestring
-except NameError:
-    string_type = str
-
-from .encode import encode
-from .decode import decode
--- src/lib/geventwebsocket/gunicorn/workers.py
+++
@@ -1,6 +0,0 @@
-from geventwebsocket.handler import WebSocketHandler
-from gunicorn.workers.ggevent import GeventPyWSGIWorker
-
-
-class GeventWebSocketWorker(GeventPyWSGIWorker):
-    wsgi_handler = WebSocketHandler
