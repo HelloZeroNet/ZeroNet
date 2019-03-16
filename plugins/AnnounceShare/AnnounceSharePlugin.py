1a9529157fa845d0376277bd64073112f7a9f775
shortcutme <tamas@zeronet.io>
2019-03-16 02:11:22 +0100

    Backward compatibility with tracker sharing response

--- plugins/AnnounceShare/AnnounceSharePlugin.py
+++ plugins/AnnounceShare/AnnounceSharePlugin.py
@@ -120,6 +120,8 @@ class TrackerStorage(object):
 
             num_success += 1
             for tracker_address in res["trackers"]:
+                if type(tracker_address) is bytes:  # Backward compatibilitys
+                    tracker_address.decode("utf8")
                 added = self.onTrackerFound(tracker_address)
                 if added:  # Only add one tracker from one source
                     break
