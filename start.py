a48b911bf0ab05b0d17aa430f33208e22398a6f2
Ivanq <imachug@yandex.ru>
2019-06-26 12:38:14 +0300

    Fix starting ZeroNet via start.py

--- start.py
+++ start.py
@@ -11,7 +11,7 @@ import zeronet
 def main():
     if "--open_browser" not in sys.argv:
         sys.argv = [sys.argv[0]] + ["--open_browser", "default_browser"] + sys.argv[1:]
-    zeronet.main()
+    zeronet.start()
 
 if __name__ == '__main__':
     main()
