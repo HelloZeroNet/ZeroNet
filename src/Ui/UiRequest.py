4cfaba79583b7f017ab42933078f2301b7c065d9
Hacktivist <57075055+hacktivist@users.noreply.github.com>
2019-10-28 15:34:33 +0000

    Update UiRequest.py

--- src/Ui/UiRequest.py
+++ src/Ui/UiRequest.py
@@ -295,11 +295,6 @@ class UiRequest(object):
             headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Cookie, Range"
             headers["Access-Control-Allow-Credentials"] = "true"
 
-        if content_type == "text/html":
-            content_type = "text/html; charset=utf-8"
-        if content_type == "text/plain":
-            content_type = "text/plain; charset=utf-8"
-
         # Download instead of display file types that can be dangerous
         if re.findall("/svg|/xml|/x-shockwave-flash|/pdf", content_type):
             headers["Content-Disposition"] = "attachment"
