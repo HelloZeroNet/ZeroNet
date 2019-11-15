5c27a0efcc9caf7d4a813e1ef07dd4d09fabea8a
shortcutme <tamas@zeronet.io>
2019-11-15 19:43:56 +0100

    Rev4260, Fix UiConfig pt-br language json

--- src/Config.py
+++ src/Config.py
@@ -13,7 +13,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.7.1"
-        self.rev = 4259
+        self.rev = 4260
         self.argv = argv
         self.action = None
         self.pending_changes = {}
