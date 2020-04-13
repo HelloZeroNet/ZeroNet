b9798c53f6144677590d932f0bf0df72bf2f9386
GoodByeZeroNet <63570881+GoodByeZeroNet@users.noreply.github.com>
2020-04-13 02:50:22 +0000

    Update CryptHash.py

--- src/Crypt/CryptHash.py
+++ src/Crypt/CryptHash.py
@@ -1,56 +1,41 @@
-import hashlib
+import blake3
 import os
 import base64
 
 
-def sha512sum(file, blocksize=65536, format="hexdigest"):
+def b3sum(file, blocksize=65536, format="hexdigest"):
     if type(file) is str:  # Filename specified
         file = open(file, "rb")
-    hash = hashlib.sha512()
+    hash = blake3.blake3()
     for block in iter(lambda: file.read(blocksize), b""):
         hash.update(block)
 
     # Truncate to 256bits is good enough
     if format == "hexdigest":
         return hash.hexdigest()[0:64]
-    else:
-        return hash.digest()[0:32]
-
-
-def sha256sum(file, blocksize=65536):
-    if type(file) is str:  # Filename specified
-        file = open(file, "rb")
-    hash = hashlib.sha256()
-    for block in iter(lambda: file.read(blocksize), b""):
-        hash.update(block)
-    return hash.hexdigest()
-
 
 def random(length=64, encoding="hex"):
     if encoding == "base64":  # Characters: A-Za-z0-9
-        hash = hashlib.sha512(os.urandom(256)).digest()
+        hash = blake3.blake3(os.urandom(256)).digest()
         return base64.b64encode(hash).decode("ascii").replace("+", "").replace("/", "").replace("=", "")[0:length]
     else:  # Characters: a-f0-9 (faster)
-        return hashlib.sha512(os.urandom(256)).hexdigest()[0:length]
+        return blake3.blake3(os.urandom(256)).hexdigest()[0:length]
 
 
-# Sha512 truncated to 256bits
-class Sha512t:
+# blake3 truncated to 256bits
+class Blake3t:
     def __init__(self, data):
         if data:
-            self.sha512 = hashlib.sha512(data)
+            self.blake3 = blake3.blake3(data)
         else:
-            self.sha512 = hashlib.sha512()
+            self.blake3 = blake3.blake3()
 
     def hexdigest(self):
-        return self.sha512.hexdigest()[0:64]
-
-    def digest(self):
-        return self.sha512.digest()[0:32]
+        return self.blake3.hexdigest()[0:64]
 
     def update(self, data):
-        return self.sha512.update(data)
+        return self.blake3.update(data)
 
 
-def sha512t(data=None):
-    return Sha512t(data)
+def blake3t(data=None):
+    return Blake3t(data)
