79d26060b3e8184a1b16a8fd7baae6ef8710b441
shortcutme <tamas@zeronet.io>
2020-06-18 17:22:33 +0200

    Add site address hash to site info websocket response

--- src/Ui/UiWebsocket.py
+++ src/Ui/UiWebsocket.py
@@ -268,6 +268,7 @@ class UiWebsocket(object):
             "cert_user_id": self.user.getCertUserId(site.address),
             "address": site.address,
             "address_short": site.address_short,
+            "address_hash": site.address_hash.hex(),
             "settings": settings,
             "content_updated": site.content_updated,
             "bad_files": len(site.bad_files),
