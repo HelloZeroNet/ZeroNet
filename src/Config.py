69d7eacfa45d1b0af53dd79fbb6bbbc81cb98e70
canewsin <canews.in@gmail.com>
2022-03-06 18:23:17 +0530

    v 0.7.9-beta (4581)

--- src/Config.py
+++ src/Config.py
@@ -13,8 +13,8 @@ import time
 class Config(object):
 
     def __init__(self, argv):
-        self.version = "0.7.8"
-        self.rev = 4580
+        self.version = "0.7.9-beta"
+        self.rev = 4581
         self.argv = argv
         self.action = None
         self.test_parser = None
