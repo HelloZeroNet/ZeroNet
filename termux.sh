ba77c99688d7a6dc61946a77b9d8dd1a0d48cd00
unmanbearpig <unmanbearpig@gmail.com>
2022-06-04 19:26:20 +0400

    termux.sh: Pull zeronet if already cloned

--- termux.sh
+++ termux.sh
@@ -1,9 +1,20 @@
 
 # Script for running zeronet-conservancy in Termux on Android
 
-git clone https://github.com/zeronet-conservancy/zeronet-conservancy
-cd zeronet-conservancy
-pkg update
-pkg install python automake git binutils tor
+if [[ -d zeronet-conservancy ]]; then
+	cd zeronet-conservancy
+	git pull --ff-only
+else
+	git clone https://github.com/zeronet-conservancy/zeronet-conservancy
+	cd zeronet-conservancy
+fi
+
+pkg update -y
+pkg install -y python automake git binutils tor
+
+echo "Starting tor..."
 tor --ControlPort 9051 --CookieAuthentication 1 >/dev/null &
+
+echo "Starting zeronet-conservancy..."
 ./start-venv.sh
+cd ..
