fd857985f61d9301c8a1cf75d0eb130daab571d1
canewsin <canews.in@gmail.com>
2022-10-01 02:22:50 +0530

    v0.8.0(4600)

--- src/Config.py
+++ src/Config.py
@@ -13,8 +13,8 @@ import time
 class Config(object):
 
     def __init__(self, argv):
-        self.version = "0.8.0"
-        self.rev = 4591
+        self.version = "0.8.1"
+        self.rev = 4600
         self.argv = argv
         self.action = None
         self.test_parser = None
